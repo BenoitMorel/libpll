873b5cb98e445ec2bfee8c2b31fa5de673786708
benoit <benomorel@gmail.com>
2017-07-12 15:57:52 +0200

    small fixes to pass more unit tests with repeats

--- test/runtest.py
+++ test/runtest.py
@@ -40,9 +40,9 @@ import time
 #####################
 #   Configuration   #
 #####################
-do_memtest       =  1                 # Evaluate memory leaks
+do_memtest       =  0                 # Evaluate memory leaks
 num_replicates   = 20                 # Number of samples for the speed test
-all_args         = [0,1,2,3,4,5,8,9]  # 0: No vector / No tip pattern
+all_args         = [18, 0,1,2,3,4,5,8,9,  # 0: No vector / No tip pattern
                                       # 1: No vector / Tip pattern
                                       # 2: AVX / No tip pattern
                                       # 3: AVX / Tip pattern
@@ -50,6 +50,7 @@ all_args         = [0,1,2,3,4,5,8,9]  # 0: No vector / No tip pattern
                                       # 5: SSE / Tip pattern
                                       # 8: AVX2 / No tip pattern
                                       # 9: AVX2 / Tip pattern
+                    18]               #18: AVX / repeats
 #####################
 
 colors={"default":"",
@@ -164,6 +165,10 @@ def runSpeedTest(files):
           attrib += " avx2"
           attribstr += " AVX2"
           typestr   += "F"
+      if (args & 16):
+          attrib    += " sr"
+          attribstr += " Site repeats"
+          typestr   += "R"
       else:
           attribstr += " CPU"
           typestr   += "C"
@@ -292,6 +297,10 @@ def runValidation(files):
           attrib    += " avx2"
           attribstr += " AVX2"
           typestr   += "F"
+      if (args & 16):
+          attrib    += " sr"
+          attribstr += "Site repeats"
+          typestr   += "R"
       else:
         attribstr += " CPU"
         typestr   += "C"
